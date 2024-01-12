f2589469e64f6ed0d4c405ac7fbafc4568de86f4
leonelborgesdev <leonel03t@gmail.com>
2024-01-11 23:57:53 -0400

    encapsulando atributos

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,34 @@ public class Person {
     private String name;
     private String age;
     private String hobby;
+
+    public Person(String name, String age, String hobby) {
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
 }
